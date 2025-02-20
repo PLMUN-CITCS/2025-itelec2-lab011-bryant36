7bab40b0058b5dbb04f4f800d06bbfb9cdaa70fb
unknown <manaladbryantray_bsit@plmun.edu.ph>
2025-02-20 01:17:47 -0800

    Submitting Python Week 04 - Laboratory # 11

---
+++ nested_if_else_statement.py
@@ -0,0 +1,24 @@
+# Bryant Ray Manalad
+# ITELEC2
+# Laboratory #09 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    try:
+        age_str = input("Enter your age: ")
+        membership_str = input("Are you a member? (Yes/No): ")
+        age = int(age_str)
+        membership = membership_str.strip().lower()
+        if age >= 18:
+            if membership == "yes":
+                print("Access granted.")
+            else:
+                print("Membership required for access.")
+        else:
+            print("Access denied. Must be at least 18 years old.")
+        #your code here
+
+    except ValueError:
+        print("Invalid age input. Please enter an integer.")
+if __name__ == "__main__":
+    main()
